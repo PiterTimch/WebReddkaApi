b2f60ca2df02a86b7c8368896fe640c8b7cbae21
Petro <petrotymchuck@gmail.com>
2025-10-23 19:56:42 +0300

    add topic migration

--- WebReddkaApi/Data/Entities/TopicEntity.cs
+++ WebReddkaApi/Data/Entities/TopicEntity.cs
@@ -7,7 +7,7 @@ namespace WebReddkaApi.Data.Entities;
 public class TopicEntity
 {
     [Column("id")]
-    public int Id { get; set; }
+    public long Id { get; set; }
     [Column("name")]
     [StringLength(200)]
     public string Name { get; set; } = string.Empty;
---
+++ WebReddkaApi/Migrations/20251023161545_Add_tblTopics.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace WebReddkaApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_tblTopics : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tblTopic",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    url_slug = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    priority = table.Column<int>(type: "integer", nullable: false),
+                    image = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    description = table.Column<string>(type: "text", nullable: true),
+                    parent_id = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblTopic", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_tblTopic_tblTopic_parent_id",
+                        column: x => x.parent_id,
+                        principalTable: "tblTopic",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tblTopic_parent_id",
+                table: "tblTopic",
+                column: "parent_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tblTopic");
+        }
+    }
+}
